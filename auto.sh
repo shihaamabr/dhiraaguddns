ee6100adad5b554c7d009318e26b01bfb52211e0
Shihaam Abdul Rahman <shihaam_ab_r@outlook.com>
2020-01-20 06:22:28 +0500

    added logging to ip.log

--- auto.sh
+++ auto.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+(
 echo ============================= ;
 echo Auto Update;
-python3 ipupdate.py ;
+/usr/bin/python3.6 ipupdate.py ;
 TZ=Indian/Maldives date; (unset TZ);
 echo =============================
+) | tee -a ip.log
